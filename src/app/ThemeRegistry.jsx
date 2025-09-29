34acf02128c90477606c18850669035806a82bf5
wahidalm <wahiduzalam@gmail.com>
2025-09-29 13:40:57 +0530

    Add fix files

---
+++ src/app/ThemeRegistry.jsx
@@ -0,0 +1,61 @@
+// app/ThemeRegistry.jsx
+'use client';
+import createCache from '@emotion/cache';
+import { useServerInsertedHTML } from 'next/navigation';
+import { CacheProvider } from '@emotion/react';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from './theme'; // We'll create this next
+import React from 'react';
+
+// This implementation is directly from MUI's official documentation for JS.
+export default function ThemeRegistry({ children }) {
+    const [{ cache, flush }] = React.useState(() => {
+        const cache = createCache({ key: 'mui' });
+        cache.compat = true;
+        const prevInsert = cache.insert;
+        let inserted = [];
+        cache.insert = (...args) => {
+            const serialized = args[1];
+            if (cache.inserted[serialized.name] === undefined) {
+                inserted.push(serialized.name);
+            }
+            return prevInsert(...args);
+        };
+        const flush = () => {
+            const prevInserted = inserted;
+            inserted = [];
+            return prevInserted;
+        };
+        return { cache, flush };
+    });
+
+    useServerInsertedHTML(() => {
+        const names = flush();
+        if (names.length === 0) {
+            return null;
+        }
+        let styles = '';
+        for (const name of names) {
+            styles += cache.inserted[name];
+        }
+        return (
+            <style
+                key={cache.key}
+                data-emotion={`${cache.key} ${names.join(' ')}`}
+                dangerouslySetInnerHTML={{
+                    __html: styles,
+                }}
+            />
+        );
+    });
+
+    return (
+        <CacheProvider value={cache}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                {children}
+            </ThemeProvider>
+        </CacheProvider>
+    );
+}
---
+++ src/app/theme.js
@@ -0,0 +1,11 @@
+// app/theme.js
+'use client';
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        mode: 'light',
+    },
+});
+
+export default theme;
